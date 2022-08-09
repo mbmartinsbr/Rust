c13dd4f3b297b481af69be5ce2dbd7e4e92486e1
Matheus Martins <matheus@signa.net.br>
2022-08-09 09:09:28 -0300

    update with first tests

---
+++ DeclareVars/declarevars.rs
@@ -0,0 +1,16 @@
+fn main() {
+    /*
+    REGRA 1 COMEÇAR COM LETRAS MINUSCULA
+    REGRA 2 TERMINAR COM LETRAS MINUSCULAS OU NUMEROS
+    OBS UNDERSCORE _ PODE SER USADO ENTRE AS LETRAS E NUMERO
+    */
+    // let msg = "olá mundo";
+    // let msg1 = "olá mundo";
+    // let Msg = "olá mundo";
+    // let 1msg = "olá mundo";
+    // let m_1_s_2_g = "olá mundo";
+
+    println!("Sua mensagem é: {}", variavel)
+
+
+}
---
+++ FirstProgram/HelloWorld.rs
@@ -0,0 +1,4 @@
+fn main(){
+    print!("Olá Mundo"); // Não pula linha
+    //println!("Olá Mundo"); // Pula linha
+}
